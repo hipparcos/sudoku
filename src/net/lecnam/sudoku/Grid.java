0fd0b7fc81aac70cbbe6f95bf88b9a323280511b
Adrien Aucher <aucher.adrien@gmail.com>
2018-05-11 14:06:36 +0200

    Grid: format class javadoc for html rendering.

--- src/net/lecnam/sudoku/Grid.java
+++ src/net/lecnam/sudoku/Grid.java
@@ -7,39 +7,42 @@ import java.io.Writer;
 import java.util.Collections;
 
 /**
- * A typical sudoku grid:
- *   Rows are designated by a letter.
- *   Columns are designated by a number.
- *   A square is the intersection of a row and a column.
- *   A unit is either a row, a column, a box.
- *   Peers are all squares contained in all units for a particular square.
- *   Each of the squares has 3 units: a row, a column and a box.
- * 
- * Rows count = 9
- * Columns count = 9
- * Squares count = 81
- * Box count = 9
- * Unit count per square = 3
- * Peers count per square = 20
- * 
+ * A typical sudoku grid:<br>
+ *   Rows are designated by a letter.<br>
+ *   Columns are designated by a number.<br>
+ *   A square is the intersection of a row and a column.<br>
+ *   A unit is either a row, a column, a box.<br>
+ *   Peers are all squares contained in all units for a particular square.<br>
+ *   Each of the squares has 3 units: a row, a column and a box.<br>
+ * <br>
+ * Rows count = 9<br>
+ * Columns count = 9<br>
+ * Squares count = 81<br>
+ * Box count = 9<br>
+ * Unit count per square = 3<br>
+ * Peers count per square = 20<br>
+ * <br>
  * Rule: A puzzle is solved if the squares in each unit are filled with a
- * permutation of the digits 1 to 9.
- * 
- *      1  2  3    4  5  6    7  8  9 
- *   ┌──────────┬──────────┬──────────┐
- * A │ A1 A2 A3 │ A4 A5 A6 │ A7 A8 A9 │
- * B │ B1 B2 B3 │ B4 B5 B6 │ B7 B8 B9 │
- * C │ C1 C2 C3 │ C4 C5 C6 │ C7 C8 C9 │
- *   ├──────────┼──────────┼──────────┤
- * D │ D1 D2 D3 │ D4 D5 D6 │ D7 D8 D9 │
- * E │ E1 E2 E3 │ E4 E5 E6 │ E7 E8 E9 │
- * F │ F1 F2 F3 │ F4 F5 F6 │ F7 F8 F9 │
- *   ├──────────┼──────────┼──────────┤
- * G │ G1 G2 G3 │ G4 G5 G6 │ G7 G8 G9 │
- * H │ H1 H2 H3 │ H4 H5 H6 │ H7 H8 H9 │
- * I │ I1 I2 I3 │ I4 I5 I6 │ I7 I8 I9 │
- *   └──────────┴──────────┴──────────┘
- *
+ * permutation of the digits 1 to 9.<br>
+ * <br>
+ * <code>
+ * &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1&nbsp;&nbsp;2&nbsp;&nbsp;3
+ * &nbsp;&nbsp;&nbsp;4&nbsp;&nbsp;5&nbsp;&nbsp;6
+ * &nbsp;&nbsp;&nbsp;7&nbsp;&nbsp;8&nbsp;&nbsp;9<br>  
+ * - ┌──────────┬──────────┬──────────┐<br>
+ * A │ A1 A2 A3 │ A4 A5 A6 │ A7 A8 A9 │<br>
+ * B │ B1 B2 B3 │ B4 B5 B6 │ B7 B8 B9 │<br>
+ * C │ C1 C2 C3 │ C4 C5 C6 │ C7 C8 C9 │<br>
+ * - ├──────────┼──────────┼──────────┤<br>
+ * D │ D1 D2 D3 │ D4 D5 D6 │ D7 D8 D9 │<br>
+ * E │ E1 E2 E3 │ E4 E5 E6 │ E7 E8 E9 │<br>
+ * F │ F1 F2 F3 │ F4 F5 F6 │ F7 F8 F9 │<br>
+ * - ├──────────┼──────────┼──────────┤<br>
+ * G │ G1 G2 G3 │ G4 G5 G6 │ G7 G8 G9 │<br>
+ * H │ H1 H2 H3 │ H4 H5 H6 │ H7 H8 H9 │<br>
+ * I │ I1 I2 I3 │ I4 I5 I6 │ I7 I8 I9 │<br>
+ * - └──────────┴──────────┴──────────┘<br>
+ * </code>
  * @author Adrien Aucher
  * 
  */
