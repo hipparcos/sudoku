bd87573c4c566a3abfba12114eb1874588f56739
Adrien Aucher <aucher.adrien@gmail.com>
2018-05-13 14:24:44 +0200

    Solver: add javadoc.

--- src/net/lecnam/sudoku/Solver.java
+++ src/net/lecnam/sudoku/Solver.java
@@ -1,5 +1,18 @@
 package net.lecnam.sudoku;
 
+/**
+ * An interface used by Grid to solve itself.<br>
+ * A solver must implement this interface.
+ *
+ * @author Adrien Aucher
+ *
+ */
 public interface Solver {
+	/**
+	 * A call to this function must solve the given grid in place.
+	 *
+	 * @param grid the grid to be solved
+	 * @return true is solving succeeded, false otherwise
+	 */
 	public boolean solve(Grid grid);
 }
